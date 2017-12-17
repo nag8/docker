85716e240031e22cd0f1492fc8021527e2b5c0ee
nag8 <nag8cool@gmail.com>
2017-12-16 18:12:12 -0600

    intial commit

---
+++ src/test/java/com/nag/kafka/MySourceConnectorConfigTest.java
@@ -0,0 +1,10 @@
+package com.nag.kafka;
+
+import org.junit.Test;
+
+public class MySourceConnectorConfigTest {
+  @Test
+  public void doc() {
+    System.out.println(MySourceConnectorConfig.conf().toRst());
+  }
+}
